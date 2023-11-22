e7384dccc42c500552884cfc1535d3191e7d3d52
Mish_Sanek <kid02@list.ru>
2023-11-22 21:05:11 +0500

    auth window

--- js/common.js
+++ js/common.js
@@ -1,3 +1,20 @@
+// sign in form
+
+const signInButton = document.querySelector('.page-header__sign-in');
+const authWindow = document.querySelector('.auth-window');
+const authWindowCloseBtn = authWindow.querySelector('.auth-window__close')
+
+signInButton.addEventListener('click', () => {
+  authWindow.classList.add('active');
+});
+
+authWindowCloseBtn.addEventListener('click', () => {
+  authWindow.classList.remove('active');
+});
+
+
+
+// mobile menu
 const headerButton = document.querySelector('.page-header__menu-btn');
 const headerNav = document.querySelector('.page-header__nav');
 
